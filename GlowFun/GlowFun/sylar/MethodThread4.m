8326bfed0e9a02fc594735eae7ffc88b74396112
sylar <sylar@Sylar-Mac-Mini-2.local>
2017-01-06 12:27:10 +0800

    thread  不过好像速度没快多少

--- GlowFun/GlowFun/sylar/MethodThread4.m
+++ GlowFun/GlowFun/sylar/MethodThread4.m
@@ -8,6 +8,10 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 #import "MethodThread4.h"
 #import "MethodReference3.h"
+#import <pthread.h>
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static NSString * const endIndexKey = @"endIndexKey";
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 @interface MethodThread4 ()
 
@@ -15,7 +19,7 @@
 @property (nonatomic, strong) NSString *endFrame;
 @property (nonatomic, assign) NSInteger maxMoveSteps;
 
-@property (nonatomic, strong) NSMutableArray *frames;  // already exist frames
+@property (atomic, strong) NSMutableArray *frames;  // already exist frames
 @property (nonatomic, strong) NSArray *currentFrames;
 @property (nonatomic, strong) NSMutableArray *nextFrames;
 
@@ -24,11 +28,20 @@
 @property (nonatomic, assign) BOOL isThreadRunning;
 
 @property (nonatomic, strong) NSMutableArray *threads;
+@property (nonatomic, assign) BOOL switchedQueue;
+@property (nonatomic, assign) NSInteger threadCount;
+@property (nonatomic, assign) NSInteger availableThreadCount;
+
 
 
 @end
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-@implementation MethodThread4
+@implementation MethodThread4 {
+    pthread_mutex_t _routesQueueMutexLock;
+    pthread_mutex_t _routesIndexMutexLock;
+    pthread_mutex_t _move;
+    pthread_mutex_t _framesLock;
+}
 
 + (instancetype)share {
     static MethodThread4 *inst = nil;
@@ -46,9 +59,13 @@
         _endFrame   = @"wbrbbrbrrbrbbrbr";
         _frames = [[NSMutableArray alloc] init];
         _maxMoveSteps = 50;
-        _frames = [[NSMutableArray alloc] init];
         _nextFrames = [[NSMutableArray alloc] init];
         _answers = [[NSMutableArray alloc] init];
+        
+        pthread_mutex_init(&_routesQueueMutexLock, NULL);
+        pthread_mutex_init(&_routesIndexMutexLock, NULL);
+        pthread_mutex_init(&_move, NULL);
+        pthread_mutex_init(&_framesLock, NULL);
     }
     return self;
 }
@@ -67,7 +84,8 @@
     
     _threads = [[NSMutableArray alloc] init];
     
-    NSInteger threadCount = 2;
+    _availableThreadCount = 4;
+    NSInteger threadCount = _availableThreadCount;
     
     for (int i=0; i<threadCount; i++) {
         NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(GO) object:nil];
@@ -103,108 +121,294 @@
         @autoreleasepool {
             
         
-        
-        if (_answers.count > 0) {
-            _isThreadRunning = NO;
-            ReferenceObje1 *aa = [_answers lastObject];
+            pthread_mutex_lock(&_routesIndexMutexLock);
             
-            NSLog(@"sylar :  answers = %@(%@, %@)", _answers, aa.steps, aa.value);
             
-            break;
-        }
+            if (_threadCount == 0) {
+                
+                if (_answers.count > 0 && _threadCount == 0) {
+                    _isThreadRunning = NO;
+                    ReferenceObje1 *aa = [_answers lastObject];
+                    
+                    NSLog(@"sylar :  answers = %@(%@, %@)", _answers, aa.steps, aa.value);
+                    
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        NSLog(@"sylar :  main.answers = %@(%@, %@)", _answers, aa.steps, aa.value);
+                    });
+                    pthread_mutex_unlock(&_routesIndexMutexLock);
+                    break;
+                } else {
+                    _switchedQueue = YES;
+                    _threadCount = _threadCount + 1;
+                    
+                    _currentFrames = [[NSArray alloc] initWithArray:_nextFrames];
+                    [_nextFrames removeAllObjects];
+                    
+                    for (NSThread *thread in _threads) {
+                        thread.threadDictionary[endIndexKey] = @0;
+                    }
+
+                }
+                
+            }
         
-        _currentFrames = [[NSArray alloc] initWithArray:_nextFrames];
-        [_nextFrames removeAllObjects];
-        
-        
-        if (_currentFrames.count > 0) {
-            ReferenceObje1 *last = [_currentFrames lastObject];
-            NSLog(@"sylar :  aaa = %ld - %ld", last.steps.length, _currentFrames.count);
             
-            if (last.steps.length == 20) {
+            pthread_mutex_unlock(&_routesIndexMutexLock);
+            
+            
+            BOOL shouldSleep = [[NSThread currentThread].threadDictionary[endIndexKey] integerValue] == -1;
+            if (shouldSleep) {
+                [NSThread sleepForTimeInterval:0];
+            } else {
+                
+                if (_currentFrames.count > 0) {
+                    ReferenceObje1 *last = [_currentFrames lastObject];
+                    NSLog(@"sylar :  aaa = %ld - %ld", last.steps.length, _currentFrames.count);
+                    
+                    if (last.steps.length == 20) {
+                        CFAbsoluteTime t2 = CFAbsoluteTimeGetCurrent();
+                        NSLog(@"sylar :  time(20) = %f", t2-t1);
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            NSLog(@"sylar :  main.time(20) = %f", t2-t1);
+                        });
+                    }
+                }
+
+                
+                pthread_mutex_lock(&_routesIndexMutexLock);
+                if (_switchedQueue == YES) {
+                    _switchedQueue = NO;
+                } else {
+                    _threadCount += 1;
+                }
+                
+                
+                int threadIndex = [[NSThread currentThread].name intValue];
+                int indexLimit = (int)(_currentFrames.count / _availableThreadCount) + 1;
+                int indexOffset = indexLimit * threadIndex;
+                int beginIndex = 0 + indexOffset;
+                int endIndex = indexOffset + (indexLimit - 1);
+                
+                NSLog(@"sylar :  thread(%ld), frames(%ld) begin-end(%ld,%ld)", threadIndex, _currentFrames.count, beginIndex, endIndex);
+                
+                pthread_mutex_unlock(&_routesIndexMutexLock);
+                
+                CFAbsoluteTime t1 = CFAbsoluteTimeGetCurrent();
+                for (int index = beginIndex; index <= endIndex; index++) {
+                    
+                    if (index >= _currentFrames.count) {
+//                        NSLog(@"sylar :  out of counttttttttt(%ld, %ld)", beginIndex, endIndex);
+                        break;
+                    }
+                    
+                    ReferenceObje1 *obj = [_currentFrames objectAtIndex:index];
+                    // left
+//                    pthread_mutex_lock(&_move);
+                    NSString *left = [obj moveLeft];
+//                    pthread_mutex_unlock(&_move);
+                    
+                    if (left) {
+                        pthread_mutex_lock(&_framesLock);
+                        BOOL contain = [_frames containsObject:left];
+                        pthread_mutex_unlock(&_framesLock);
+                        if (contain == NO) {
+                            // new frame
+                            pthread_mutex_lock(&_framesLock);
+                            [_frames addObject:left];
+                            pthread_mutex_unlock(&_framesLock);
+                            
+                            ReferenceObje1 *newObj = [[ReferenceObje1 alloc] init];
+                            newObj.value = left;
+                            newObj.steps = [NSString stringWithFormat:@"%@L", obj.steps];
+                            pthread_mutex_lock(&_routesQueueMutexLock);
+                            [_nextFrames addObject:newObj];
+                            pthread_mutex_unlock(&_routesQueueMutexLock);
+                            
+                            if ([left isEqualToString:_endFrame]) {
+                                [_answers addObject:newObj];
+                            }
+                        }
+                    }
+                    
+                    // right
+//                    pthread_mutex_lock(&_move);
+                    NSString *right = [obj moveRight];
+//                    pthread_mutex_unlock(&_move);
+                    if (right) {
+                        pthread_mutex_lock(&_framesLock);
+                        BOOL contain = [_frames containsObject:right];
+                        pthread_mutex_unlock(&_framesLock);
+                        if (contain == NO) {
+                            // new frame
+                            pthread_mutex_lock(&_framesLock);
+                            [_frames addObject:right];
+                            pthread_mutex_unlock(&_framesLock);
+                            
+                            ReferenceObje1 *newObj = [[ReferenceObje1 alloc] init];
+                            newObj.value = right;
+                            newObj.steps = [NSString stringWithFormat:@"%@R", obj.steps];
+                            pthread_mutex_lock(&_routesQueueMutexLock);
+                            [_nextFrames addObject:newObj];
+                            pthread_mutex_unlock(&_routesQueueMutexLock);
+
+                            
+                            if ([right isEqualToString:_endFrame]) {
+                                [_answers addObject:newObj];
+                            }
+                        }
+                    }
+                    
+                    // down
+//                    pthread_mutex_lock(&_move);
+                    left = [obj moveDown];
+//                    pthread_mutex_unlock(&_move);
+                    if (left) {
+                        pthread_mutex_lock(&_framesLock);
+                        BOOL contain = [_frames containsObject:left];
+                        pthread_mutex_unlock(&_framesLock);
+                        if (contain == NO) {
+                            // new frame
+                            pthread_mutex_lock(&_framesLock);
+                            [_frames addObject:left];
+                            pthread_mutex_unlock(&_framesLock);
+                            
+                            ReferenceObje1 *newObj = [[ReferenceObje1 alloc] init];
+                            newObj.value = left;
+                            newObj.steps = [NSString stringWithFormat:@"%@D", obj.steps];
+                            pthread_mutex_lock(&_routesQueueMutexLock);
+                            [_nextFrames addObject:newObj];
+                            pthread_mutex_unlock(&_routesQueueMutexLock);
+
+                            
+                            if ([left isEqualToString:_endFrame]) {
+                                [_answers addObject:newObj];
+                            }
+                        }
+                    }
+                    
+                    // up
+//                    pthread_mutex_lock(&_move);
+                    left = [obj moveUp];
+//                    pthread_mutex_unlock(&_move);
+                    if (left) {
+                        pthread_mutex_lock(&_framesLock);
+                        BOOL contain = [_frames containsObject:left];
+                        pthread_mutex_unlock(&_framesLock);
+                        if (contain == NO) {
+                            // new frame
+                            pthread_mutex_lock(&_framesLock);
+                            [_frames addObject:left];
+                            pthread_mutex_unlock(&_framesLock);
+                            
+                            ReferenceObje1 *newObj = [[ReferenceObje1 alloc] init];
+                            newObj.value = left;
+                            newObj.steps = [NSString stringWithFormat:@"%@U", obj.steps];
+                            pthread_mutex_lock(&_routesQueueMutexLock);
+                            [_nextFrames addObject:newObj];
+                            pthread_mutex_unlock(&_routesQueueMutexLock);
+
+                            
+                            if ([left isEqualToString:_endFrame]) {
+                                [_answers addObject:newObj];
+                            }
+                        }
+                    }
+                    
+                }
                 CFAbsoluteTime t2 = CFAbsoluteTimeGetCurrent();
-                NSLog(@"sylar :  time = %f", t2-t1);
-            }
-        }
-        
-        
-        for (ReferenceObje1 *obj in _currentFrames) {
-            
-            // left
-            NSString *left = [obj moveLeft];
-            if (left) {
-                if ([_frames containsObject:left] == NO) {
-                    // new frame
-                    [_frames addObject:left];
-                    
-                    ReferenceObje1 *newObj = [[ReferenceObje1 alloc] init];
-                    newObj.value = left;
-                    newObj.steps = [NSString stringWithFormat:@"%@L", obj.steps];
-                    [_nextFrames addObject:newObj];
-                    
-                    if ([left isEqualToString:_endFrame]) {
-                        [_answers addObject:newObj];
-                    }
-                }
+                
+                NSLog(@"sylar :  time = %f (begin(%ld)-end(%ld))", t2-t1, beginIndex, endIndex);
+                // Finished this round, put thread into sleep
+                [NSThread currentThread].threadDictionary[endIndexKey] = @(-1);
+                pthread_mutex_lock(&_routesIndexMutexLock);
+                _threadCount -= 1;
+                pthread_mutex_unlock(&_routesIndexMutexLock);
+                
+                
             }
             
-            // right
-            NSString *right = [obj moveRight];
-            if (right) {
-                if ([_frames containsObject:right] == NO) {
-                    // new frame
-                    [_frames addObject:right];
-                    
-                    ReferenceObje1 *newObj = [[ReferenceObje1 alloc] init];
-                    newObj.value = right;
-                    newObj.steps = [NSString stringWithFormat:@"%@R", obj.steps];
-                    [_nextFrames addObject:newObj];
-                    
-                    if ([right isEqualToString:_endFrame]) {
-                        [_answers addObject:newObj];
-                    }
-                }
-            }
             
-            // down
-            left = [obj moveDown];
-            if (left) {
-                if ([_frames containsObject:left] == NO) {
-                    // new frame
-                    [_frames addObject:left];
-                    
-                    ReferenceObje1 *newObj = [[ReferenceObje1 alloc] init];
-                    newObj.value = left;
-                    newObj.steps = [NSString stringWithFormat:@"%@D", obj.steps];;
-                    [_nextFrames addObject:newObj];
-                    
-                    if ([left isEqualToString:_endFrame]) {
-                        [_answers addObject:newObj];
-                    }
-                }
-            }
             
-            // up
-            left = [obj moveUp];
-            if (left) {
-                if ([_frames containsObject:left] == NO) {
-                    // new frame
-                    [_frames addObject:left];
-                    
-                    ReferenceObje1 *newObj = [[ReferenceObje1 alloc] init];
-                    newObj.value = left;
-                    newObj.steps = [NSString stringWithFormat:@"%@U", obj.steps];;
-                    [_nextFrames addObject:newObj];
-                    
-                    if ([left isEqualToString:_endFrame]) {
-                        [_answers addObject:newObj];
-                    }
-                }
-            }
-        }
-        
-        
-    }  // auto release pool
+            
+            
+            
+//            for (ReferenceObje1 *obj in _currentFrames) {
+//                
+//                // left
+//                NSString *left = [obj moveLeft];
+//                if (left) {
+//                    if ([_frames containsObject:left] == NO) {
+//                        // new frame
+//                        [_frames addObject:left];
+//                        
+//                        ReferenceObje1 *newObj = [[ReferenceObje1 alloc] init];
+//                        newObj.value = left;
+//                        newObj.steps = [NSString stringWithFormat:@"%@L", obj.steps];
+//                        [_nextFrames addObject:newObj];
+//                        
+//                        if ([left isEqualToString:_endFrame]) {
+//                            [_answers addObject:newObj];
+//                        }
+//                    }
+//                }
+//                
+//                // right
+//                NSString *right = [obj moveRight];
+//                if (right) {
+//                    if ([_frames containsObject:right] == NO) {
+//                        // new frame
+//                        [_frames addObject:right];
+//                        
+//                        ReferenceObje1 *newObj = [[ReferenceObje1 alloc] init];
+//                        newObj.value = right;
+//                        newObj.steps = [NSString stringWithFormat:@"%@R", obj.steps];
+//                        [_nextFrames addObject:newObj];
+//                        
+//                        if ([right isEqualToString:_endFrame]) {
+//                            [_answers addObject:newObj];
+//                        }
+//                    }
+//                }
+//                
+//                // down
+//                left = [obj moveDown];
+//                if (left) {
+//                    if ([_frames containsObject:left] == NO) {
+//                        // new frame
+//                        [_frames addObject:left];
+//                        
+//                        ReferenceObje1 *newObj = [[ReferenceObje1 alloc] init];
+//                        newObj.value = left;
+//                        newObj.steps = [NSString stringWithFormat:@"%@D", obj.steps];;
+//                        [_nextFrames addObject:newObj];
+//                        
+//                        if ([left isEqualToString:_endFrame]) {
+//                            [_answers addObject:newObj];
+//                        }
+//                    }
+//                }
+//                
+//                // up
+//                left = [obj moveUp];
+//                if (left) {
+//                    if ([_frames containsObject:left] == NO) {
+//                        // new frame
+//                        [_frames addObject:left];
+//                        
+//                        ReferenceObje1 *newObj = [[ReferenceObje1 alloc] init];
+//                        newObj.value = left;
+//                        newObj.steps = [NSString stringWithFormat:@"%@U", obj.steps];;
+//                        [_nextFrames addObject:newObj];
+//                        
+//                        if ([left isEqualToString:_endFrame]) {
+//                            [_answers addObject:newObj];
+//                        }
+//                    }
+//                }
+//            }
+            
+            
+        }  // auto release pool
     }
     
 }
